ad5cce1f8cda473305a5b41b15ac7ca0963462f8
Gustavo Dias <90855105+gustapine@users.noreply.github.com>
2022-10-27 10:58:32 -0300

    Create exx02.py

---
+++ exx02.py
@@ -0,0 +1,24 @@
+#2) Qual é custodia total?
+
+##########
+import numpy as np
+import pandas as pd
+
+#define dataframe and column values
+df = pd.DataFrame({
+   'custodia' : [60.000,
+ 50000.00,
+ 123000.00, 
+ 800500.00, 
+ 800500.00, 
+ 20000.00,
+ 40000.00,   
+ 50000.00,
+ 100000.00, 
+ 24242.00, 
+ 24242.00, 
+ 24242.00]})
+ 
+#define local total of the column
+df.loc['Total'] = df.sum()
+print(df)
